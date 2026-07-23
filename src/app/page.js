ba31e709cea1ba0b0376400f199a2815e60b8340
jema <smh1141@naver.com>
2026-07-23 10:44:09 +0900

    feat: Home에 정적 이미지 추가

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
   console.log("홈 페이지 동작");
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <h1>Next.js</h1>
       <p>Hello, world</p>
+      <Image src="/home.png" alt="Picture of the author" width={50} height={50} />
     </>
   );
 }
