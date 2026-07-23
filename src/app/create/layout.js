2f3d6e9f5a4b3051bca05acf50132b090097532b
jema <smh1141@naver.com>
2026-07-23 11:11:00 +0900

    feat: 스크립트 로드 및 스타일 모듈로 적용해보기

--- src/app/create/layout.js
+++ src/app/create/layout.js
@@ -1,8 +1,9 @@
+import styles from "./create.module.css";
 export default function createLayout({ children }) {
   console.log("생성 레이아웃 동작");
   return (
     <form>
-      <h2>Create layout</h2>
+      <h2 className={styles.title}>Create layout</h2>
       {children}
     </form>
   );
